1f6b3c6f1d7c150038f2eeb255125a05562bd103
mackiexia <lamei2601@outlook.com>
2022-09-26 21:48:21 +0800

    1st commit

---
+++ client/app.js
@@ -0,0 +1,47 @@
+const tableBody = document.getElementById("table-body");
+
+const getFlight = () => {
+  fetch("http://localhost:8000/flights")
+    .then((response) => response.json())
+    .then((flights) => {
+      populateTable(flights);
+    })
+    .catch((err) => console.log(err));
+};
+
+getFlight();
+
+const populateTable = (flights) => {
+  for (const flight of flights) {
+    const tableRow = document.createElement("tr");
+    const tableData = document.createElement("td");
+
+    tableData.textContent = "✈";
+    tableRow.append(tableData);
+
+    const flightDetails = {
+      time: flight.departing.slice(0, 6),
+      destination: flight.destination.toUpperCase(),
+      flight: flight.flightNumber.shift(),
+      gate: flight.gate,
+      remarks: flight.status.toUpperCase(),
+    };
+
+    for (const flightDetail in flightDetails) {
+      const tableCell = document.createElement("td");
+      const word = Array.from(flightDetails[flightDetail]);
+
+      for (const [index, letter] of word.entries()) {
+        const letterElement = document.createElement("div");
+
+        setTimeout(() => {
+          letterElement.classList.add("flip");
+          letterElement.textContent = letter;
+          tableCell.append(letterElement);
+        }, 100 * index);
+      }
+      tableRow.append(tableCell);
+    }
+    tableBody.append(tableRow);
+  }
+};
